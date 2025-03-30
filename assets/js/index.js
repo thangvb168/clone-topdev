341f65b92ca39c5e247a512b107e7d4e98b48ea9
vbthang <2151062875@e.tlu.edu.vn>
2025-03-30 12:19:34 +0700

    feat: add component slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,20 @@ const tab1 = Tab({
 const tab2 = Tab({
     tabsClass: 'js-tabs-vertical',
 }).init();
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    stagePadding: 200,
+    nav: false,
+    dots: true,
+    items: 3,
+});
+
+$('#customPrev').click(function () {
+    owl.trigger('prev.owl.carousel');
+});
+$('#customNext').click(function () {
+    owl.trigger('next.owl.carousel');
+});
