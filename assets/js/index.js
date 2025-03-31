f6344e34735632a14bcc3e8bcc3c818962f44c13
vbthang <2151062875@e.tlu.edu.vn>
2025-03-31 13:29:11 +0700

    feat: add layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,79 @@
 import Tab from './src/tab.js';
 
 const tab1 = Tab({
-    tabsClass: 'js-tabs-align-center',
+    tabsClass: 'js-tabs-job',
 }).init();
 
-const tab2 = Tab({
-    tabsClass: 'js-tabs-vertical',
-}).init();
+// const tab2 = Tab({
+//     tabsClass: 'js-tabs-vertical',
+// }).init();
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
+// var owl = $('.owl-carousel');
+// owl.owlCarousel({
+//     loop: true,
+//     margin: 32,
+//     // stagePadding: 200,
+//     nav: false,
+//     autoWidth: true,
+//     // dots: true,
+//     // items: 5,
+// });
+$('.owl-carousel-hero').owlCarousel({
     loop: true,
     margin: 10,
-    stagePadding: 200,
-    nav: false,
+    items: 2,
+});
+
+$('.owl-carousel-brand').owlCarousel({
+    loop: true,
+    margin: 32,
+    autoWidth: true,
+});
+
+$('.owl-carousel-card-job-dots').owlCarousel({
+    loop: true,
+    margin: 32,
+    autoWidth: true,
+    center: true,
+    items: 1,
     dots: true,
-    items: 3,
+});
+
+$('.owl-carousel-featured-companies').owlCarousel({
+    loop: true,
+    margin: 32,
+    autoWidth: true,
+});
+
+const owlCarouselHotJob = $('.owl-carousel-hot-job');
+owlCarouselHotJob.owlCarousel({
+    loop: true,
+    margin: 32,
+    autoWidth: true,
+    center: true,
+});
+
+const owlCarouselBlog = $('.owl-carousel-blog');
+owlCarouselBlog.owlCarousel({
+    loop: true,
+    margin: 16,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
 });
 
 $('#customPrev').click(function () {
-    owl.trigger('prev.owl.carousel');
+    owlCarouselHotJob.trigger('prev.owl.carousel');
 });
+
 $('#customNext').click(function () {
-    owl.trigger('next.owl.carousel');
+    owlCarouselHotJob.trigger('next.owl.carousel');
 });
