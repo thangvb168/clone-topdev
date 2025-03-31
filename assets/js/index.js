fcaf2605574d41af5bb3a95ed615523a1fcd31ec
vbthang <2151062875@e.tlu.edu.vn>
2025-03-31 14:51:14 +0700

    fix: add more slider to reduce broke layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,12 +22,16 @@ $('.owl-carousel-hero').owlCarousel({
     loop: true,
     margin: 10,
     items: 2,
+    nav: false,
+    dots: false,
 });
 
 $('.owl-carousel-brand').owlCarousel({
     loop: true,
     margin: 32,
     autoWidth: true,
+    nav: false,
+    dots: false,
 });
 
 $('.owl-carousel-card-job-dots').owlCarousel({
@@ -43,6 +47,8 @@ $('.owl-carousel-featured-companies').owlCarousel({
     loop: true,
     margin: 32,
     autoWidth: true,
+    nav: false,
+    dots: false,
 });
 
 const owlCarouselHotJob = $('.owl-carousel-hot-job');
@@ -51,12 +57,16 @@ owlCarouselHotJob.owlCarousel({
     margin: 32,
     autoWidth: true,
     center: true,
+    nav: false,
+    dots: false,
 });
 
 const owlCarouselBlog = $('.owl-carousel-blog');
 owlCarouselBlog.owlCarousel({
     loop: true,
     margin: 16,
+    nav: true,
+    dots: true,
     responsive: {
         0: {
             items: 1,
@@ -76,4 +86,30 @@ $('#customPrev').click(function () {
 
 $('#customNext').click(function () {
     owlCarouselHotJob.trigger('next.owl.carousel');
+});
+
+const owlCarouselHeroFeatures = $('.owl-carousel-hero-features');
+owlCarouselHeroFeatures.owlCarousel({
+    margin: 12,
+    autoWidth: true,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 2,
+            loop: true,
+        },
+        992: {
+            loop: false,
+            center: true,
+        },
+    },
+});
+
+const owlCarouselTabs = $('.owl-carousel-tabs');
+owlCarouselTabs.owlCarousel({
+    margin: 8,
+    autoWidth: true,
+    nav: false,
+    dots: false,
 });
